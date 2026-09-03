f0d54b1b671f243da8a9838d0d7ad00335de77b9
Gavin Burgess <burgessgavin23@gmail.com>
2026-09-03 18:59:22 -0400

    Add files via upload

---
+++ GavinBio.java
@@ -0,0 +1,7 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Name: Gavin Burgess");
+        System.out.println("Age: 22");
+        System.out.println("Expected First Job: Web Developer or Software Developer");
+    }
+}
