b984b47f2f8b5d27f3cd582714ca3e27daeb9826
Corey Verkouteren <90060114+clodon2@users.noreply.github.com>
2026-09-03 15:44:46 -0400

    add corey bio and java info program

---
+++ CoreyInfo.java
@@ -0,0 +1,13 @@
+public class CoreyInfo {
+    public static void main(String[] args) {
+        String[] info = {
+            "Corey Verkouteren",
+            "Computer Science", 
+            "I will probably technically be a teacher's assistant since I plan on getting a graduate degree or PHD.", 
+            "After that though, I would like to work in machine learning applications."
+        };
+        for (String message : info) {
+            System.out.println(message);
+        }
+    }
+}
