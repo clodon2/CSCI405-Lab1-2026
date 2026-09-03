f28a7ba8c77016273f208bed256c11240e00d97c
Josh Cruz <jac010@marietta.edu>
2026-09-03 15:42:32 -0400

    Add files via upload

---
+++ CruzLab1_Main.java
@@ -0,0 +1,7 @@
+public class CruzLab1_Main {
+  public static void main(String[] args) {
+    System.out.println("Name: Joshua Cruz");
+    System.out.println("Major: Cybersecurity");
+    System.out.println("Job after college: Geek Squad Consultation Agent");
+  }
+}
