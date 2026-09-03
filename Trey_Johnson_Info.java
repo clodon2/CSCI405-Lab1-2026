3803af6e359d4e94c2322cc17afbbfbba2238b87
Trey Johnson <treyjohnson25@gmail.com>
2026-09-03 15:53:16 -0400

    I am adding my Bio.txt file and my Info.java file to the repository.

---
+++ Trey_Johnson_Info.java
@@ -0,0 +1,7 @@
+public class TreyJohnson {
+    public static void main(String[] args) {
+        System.out.println("Name: Trey Johnson");
+        System.out.println("Major: Computer Science and Cybersecurity");
+        System.out.println("Expected First Job: Network Engineer");
+    }
+}
