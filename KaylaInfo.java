136072a9750eaadb1f0af8154652e9a5339499e7
kmp005 <kmp005@marietta.edu>
2026-09-03 23:34:30 -0400

    Added Kayla Java Info

---
+++ KaylaInfo.java
@@ -0,0 +1,7 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Name: Kayla Peterson");
+        System.out.println("Major: Computer Science and Graphic Design");
+        System.out.println("Expected First Job: UX/UI Designer or Product Designer");
+    }
+}
