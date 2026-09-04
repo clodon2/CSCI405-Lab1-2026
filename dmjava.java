b15a65b031ec07765361853e0eb0e6a327235814
nihiln <mdrew6663@gmail.com>
2026-09-03 21:09:21 -0400

    Add files via upload

---
+++ dmjava.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hi, I'm Drew Martin.\n
+    I am a student at Marietta College.\n 
+    I plan on working in Full Stack Development out of college.");
+  }
+}
